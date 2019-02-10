446bfec6a65124c92973c86078bf6e2f5a1e7c9d
smellslikegithub <ashish.m.husain@gmail.com>
2019-02-10 20:38:59 +0100

    staging files

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import systemInfoClass
 def main():
     info = systemInfoClass.SystemInfo()
-    info.getInfo() 
-    
+    info.showAllInfo() 
+
+   
+
 if __name__ == "__main__":
     main()
--- systemInfoClass.py
+++ systemInfoClass.py
@@ -3,11 +3,23 @@ import datetime
 
 class SystemInfo:
     #variables
+    infoDict = {
+        "timestamp":"",
+        "machine_model":"hello"
+    }
     timeStamp = 0
 
     def __init__(self):
-        self.timeStamp = datetime.datetime
+        self.timeStamp = datetime.datetime.utcnow()
     
+    def __getCpuInfo(self):
+        with open("/proc/cpuinfo","r") as i:
+            cpuInfo = i.readlines()
+            for i in cpuInfo:
+                i.strip().split(":")
+                if("model name" in i):
+                    self.infoDict["machine_model"] = i
     
-    def getInfo(self):
-        print(self.timeStamp)
+    def showAllInfo(self):
+        self.__getCpuInfo()
+        print(self.infoDict["machine_model"])
