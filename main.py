138b20ba647e80dbb1393e67fb2c022f6a3af3a6
smellslikegithub <ashish.m.husain@gmail.com>
2019-02-10 21:58:09 +0100

    extracts system info successfully

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ def main():
     info = systemInfoClass.SystemInfo()
     info.showAllInfo() 
 
-   
-
 if __name__ == "__main__":
     main()
--- systemInfoClass.py
+++ systemInfoClass.py
@@ -4,8 +4,13 @@ import datetime
 class SystemInfo:
     #variables
     infoDict = {
-        "timestamp":"",
-        "machine_model":"hello"
+        "memory": None,
+        "freeMemory": None,
+        "osDist": None,
+        "system": None,
+        "architecture":None,
+        "machine":None,
+        "node":None
     }
     timeStamp = 0
 
@@ -19,7 +24,37 @@ class SystemInfo:
                 i.strip().split(":")
                 if("model name" in i):
                     self.infoDict["machine_model"] = i
+
+    def __getMemoryInfo(self):
+        with open("/proc/meminfo", "r") as i:
+            memInfo = i.readlines()
+            self.infoDict["memory"] = memInfo[0]
+            self.infoDict["freeMemory"] = memInfo[1]
+            
+    def __operatingfSystemDistribution(self):
+        self.infoDict["osDist"] = p.dist()
+        
+    def __systemInfo(self):
+        self.infoDict["system"] = p.system()
+
+    def __architecture(self):
+        self.infoDict["architecture"] = p.architecture()[0]
     
+    def __machine(self):
+        self.infoDict["machine"] = p.machine()
+    
+    def __node(self):
+        self.infoDict["node"] = p.node()
+
     def showAllInfo(self):
         self.__getCpuInfo()
-        print(self.infoDict["machine_model"])
+        self.__getMemoryInfo()
+        self.__operatingfSystemDistribution()
+        self.__systemInfo()
+        self.__architecture()
+        self.__machine()
+        self.__node()
+
+        for key, value in self.infoDict.items():
+            print (str(key) , str(value))
+    
